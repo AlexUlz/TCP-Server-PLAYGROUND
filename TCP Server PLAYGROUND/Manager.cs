f5b2c1ab6c3d2e9ec41cf19db811d32ad914ac03
AlexUlz <117368695+AlexUlz@users.noreply.github.com>
2024-03-13 21:13:39 +0100

    Projektdateien hinzufügen.

---
+++ TCP Server PLAYGROUND/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Server_PLAYGROUND
+{
+    internal class Manager
+    {
+        private string username { get; set; }
+        private Guid id { get; set; }
+        private TcpClient userClient { get; set; }
+
+
+        public Manager(TcpClient client) 
+        {
+            userClient = client;
+            id = Guid.NewGuid();
+
+            Console.WriteLine("<{0}> {1} has connected to the server", DateTime.Now, username);
+
+        }
+
+    }
+}
---
+++ TCP Server PLAYGROUND/TCPServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Server_PLAYGROUND
+{
+    public class TCPServer
+    {
+        static TcpListener _listener;
+        static List<Manager> _users;
+        public static void Main(string[] args)
+        {
+            _users = new List<Manager>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            var Client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected!");
+            
+            while (true)
+            {
+                var newClient = new Manager(_listener.AcceptTcpClient());
+                _users.Add(newClient);
+            }
+        }
+    }
+}
